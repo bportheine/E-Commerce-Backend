1592e8c3722fee188592b8599e56b728459c5486
Brandon Portheine <bportheine@gmail.com>
2018-03-02 09:31:11 +0100

    change the port listen and sequelize port for heroku

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 
 var Sequelize = require('sequelize')
-var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
+var sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://porstgres:secret@localhost:5432/postgres')
 
-app.listen(4001, () => console.log('Express API listening on port 4001'))
+app.listen(process.env.PORT || 4001, () => console.log('Express API listening on port 4001'))
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
